8809181b2f6a6f0984aea7c95e5571e30d375e70
Milo Knowles <mknowles@mit.edu>
2016-12-03 19:28:55 -0500

    Updated some params

--- bluesyRNN-v1.py
+++ bluesyRNN-v1.py
@@ -10,7 +10,6 @@ from neuralmodels.updates import Adagrad
 from neuralmodels.costs import softmax_loss
 
 
-
 # Input vector encoding
 # [4] encoding of current beat (4 bit binary to represent 16 possible beats from 4/4 time)
 # [1] fraction of current location in form (ex. 2nd measure of 4)
@@ -18,11 +17,16 @@ from neuralmodels.costs import softmax_loss
 # [12] chord tones (-1 if not, 1 if chord tone)
 # [120] notes held or articulated previous timestep, C1-C6 possibly values (60 notes across 5 octaves)
 
+# OUTPUT #
+# [120] notes held or articulated previous timestep, C1-C6 possibly values (60 notes across 5 octaves)
+# [1] a silent output that blocks all other notes
+
 
 # PARAMETERS #
 inputDim = 138
 initial_step_size = 1e-3 # the initial learning rate
-numClasses = 120 # 60 different pitches, from C1 to C6, and articulation bool
+numPitchClasses = 43 # 60 different pitches, from low E on guitar to highest Bflat
+midiRange = range(40, 83) #43 notes we can choose from [40, 82]
 
 
 # wtf is this
