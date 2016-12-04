12a8ac27c5b585322d8612471a8730ed3211d51b
Milo Knowles <mknowles@mit.edu>
2016-12-03 19:29:09 -0500

    Can now play a backing track while waiting for midi events

--- midiInput.py
+++ midiInput.py
@@ -1,5 +1,8 @@
 import pygame
 from pygame import midi
+from pygame.mixer import music
+import pygame.mixer
+import midi as m
 
 
 def checkForDevices():
@@ -13,18 +16,51 @@ def checkForDevices():
 		return False
 
 
+def playMusic(music_file, num_repeats):
+    """
+    stream music with mixer.music module in blocking manner
+    this will stream the sound from disk while playing
+    """
+    clock = pygame.time.Clock()
+    pygame.mixer.init()
+    try:
+        music.load(music_file)
+        print "Music file %s loaded!" % music_file
+    except pygame.error:
+        print "File %s not found! (%s)" % (music_file, pygame.get_error())
+        return
+
+    music.play(loops=num_repeats)
+
+    #while music.get_busy():
+        # check if playback has finished
+        #clock.tick(30)
+
+
+
+# MIDI PACKET FORMAT #
 # [[[status,data1,data2,data3],timestamp]
-# data3: channel (0)
-# 
+# ex. NOTE ON [[[144, note_value, velocity], time (millis)]]
+# ex. NOTE OFF [[[128, note_value, velocity = 0], time (millis)]]
 
 
 def main():
+	#pygame.init()
+	organ_file = './Dataset/mids/final_proj_organ.mid'
+	bass_file = './Dataset/mids/final_proj_bass.mid'
+	music_file = 'happy_bflat_blues.ogg'
+	tempo = 56 # quarters per minute
+
+
 	midi.init()
 	
 	if checkForDevices() == True:
 		guitar = midi.Input(0, 4)
 	else:
 		print "No MIDI output found. Make sure a virtual MIDI output exists."
+
+	# play the backing track
+	playMusic(music_file, 4)
 
 	try:
 		while(True):
@@ -36,7 +72,12 @@ def main():
 	except(KeyboardInterrupt):
 		guitar.close()
 		midi.quit()
+		pygame.quit()
 
+# align recording with tempo
+# play the backing midi track with software instruments
+# extract relevant information from the midi backing track
+# at each timestep, collect an input vector, and an output vector (the note that is heard)
 
 if __name__ == '__main__':
 	main()
