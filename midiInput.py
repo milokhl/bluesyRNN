867ab38f6fdaeecacec35c3dee2bf1e1e3e929a6
Milo Knowles <mknowles@mit.edu>
2016-12-10 17:32:19 -0500

    Use port.poll() so loop is blocked while waiting for msg

--- midiInput.py
+++ midiInput.py
@@ -20,9 +20,7 @@ def checkForDevices():
 
 def playMusic(music_file, num_repeats):
     """
-    Note: music_file should be a .ogg
-    stream music with mixer.music module in blocking manner
-    this will stream the sound from disk while playing
+    Note: music_file should be a .ogg filetype
     """
     clock = pygame.time.Clock()
     pygame.mixer.init()
@@ -36,17 +34,32 @@ def playMusic(music_file, num_repeats):
     music.play(loops=num_repeats)
     return True
 
+def constructMidiArray(num_measures, beats_per_measure, ticks_per_beat):
+	"""
+	beats_per_measure: # quarter notes in a measure (for 6/8 time, there are 3 quarter notes)
+	ticks_per_beat: # of ticks we want slots for at each beat (6 ticks/beat means 3 ticks for each eighth note)
+	num_measures: total # of measures in the form (i.e 8 or 12)
 
-def printMIDIMsg(msg):
-	print msg
-
-def constructMidiArray(num_voices, num_measures, beats_per_measure, ticks_per_beat):
+	Builds an array with a slot for each tick in the form.
+	At each tick, stores another array of MIDI events, to allow for multiple notes at a time.
+	"""
 	total_ticks = num_measures*beats_per_measure*ticks_per_beat
 	midiArray = [[] for i in range(total_ticks)]
 	print("Constructed midi array with", total_ticks, "ticks.")
 	return midiArray
 
-def buildMidiFileFromArray(midi_array,bpm=56,ticks_per_beat=6,sig_num=6, sig_den=8, tempo_factor=0.5):
+def buildMidiFileFromArray(midi_array, filename='new_song.mid', bpm=56,ticks_per_beat=6,sig_num=6, sig_den=8, tempo_factor=0.5, min_note=40, max_note=82):
+	"""
+	midi_array: an array of midi events, indexed by tick
+	sig_num: the numerator of the time signature
+	sig_den: the denominator of the time signature
+	tempo_factor: used to scale the speed of the midifile (use 0.5 to account for 6/8 vs. 6/4 weirdness)
+	min_note: the lowest note that will be allowed
+	max_note: the highest note that will be allowed
+
+	Builds the midi file, saves it to the current directory, and returns it.
+	"""
+	#set up the midifile
 	mid = MidiFile(type=0)
 	mid.ticks_per_beat = ticks_per_beat
 	track = MidiTrack()
@@ -66,66 +79,72 @@ def buildMidiFileFromArray(midi_array,bpm=56,ticks_per_beat=6,sig_num=6, sig_den
 			continue
 		else:
 			for event in midi_array[tick]:
-				event.time = tick - last_active_tick
-				last_active_tick = tick
-				track.append(event)
+				# check if notes are within guitar playable range; if not, ignore
+				if event.note > min_note and event.note < max_note:
+					event.time = tick - last_active_tick
+					last_active_tick = tick
+					track.append(event)
+
+	# turn off every note to make sure we don't leave anything playing infinitely
+	for pitch in range(40,83):
+		off_msg = Message('note_off', note=pitch, velocity=0, time=1)
+		track.append(off_msg)
+
 	# save the file to memory: will overwrite!
-	mid.save('new_song.mid')
+	mid.save(filename)
 	return mid
 
 
 def main():
+	# get midi files from backing track
 	organ_file = './Dataset/mids/final_proj_organ.mid'
 	bass_file = './Dataset/mids/final_proj_bass.mid'
 	music_file = 'happy_bflat_blues.ogg'
+
+
+	### DEFINE PARAMETERS ###
 	bpm = 56 # quarters per minute
-	tempo = bpm2tempo(bpm)
-	print tempo
+	quarters_per_measure = 3
+	measures_in_form = 8
+	tempo = bpm2tempo(bpm) #this is the number of microseconds in a beat
+	micros_per_beat = bpm2tempo(bpm)
+	ticks_per_beat = 6
+	sig_num = 6
+	sig_den = 8
+	### END DEFINES ###
 
 	# create a midi file to store the recording session to
-	mid = MidiFile(type=0)
-	mid.ticks_per_beat = 6
+	mid = MidiFile(type=0) #type 0 is a single track
+	mid.ticks_per_beat = ticks_per_beat
 	track = MidiTrack()
 	mid.tracks.append(track)
-	micros_per_beat = bpm2tempo(bpm)
 	tempo_msg = MetaMessage('set_tempo', tempo=bpm2tempo(bpm))
-	time_sig_msg = MetaMessage('time_signature', numerator=6, denominator=8)
+	time_sig_msg = MetaMessage('time_signature', numerator=sig_num, denominator=sig_den)
 	track.append(tempo_msg)
 	track.append(time_sig_msg)
-	# track.append(Message('note_on', note=64, velocity=64, time=2))
-	# track.append(Message('note_off', note=64, velocity=127, time=2))
-	# right now just saving a couple notes as a test #
-
 
 	# create an array to store all midi objects in
-	midArr = constructMidiArray(6,8,3,6)
+	midArr = constructMidiArray(measures_in_form,quarters_per_measure,ticks_per_beat)
 
 	# play the backing track
 	playMusic(music_file, 0)
 
 	# open a port to receive midi events
 	port = mido.open_input()
-	recordTime = 22 # sec
+	recordTime = float(micros_per_beat)/1e6 * quarters_per_measure * measures_in_form
+	print("Will record input for:", recordTime)
 	startTime = time.time()
 
-	#keep track of when last midi message happened
-	prev_msg_stamp_millis = startTime
-
+	# while we still have recording time
 	currentTick = 0
-	while(currentTick < 144):
-
-		#look for available messages
-		msg = port.receive()
-		# msg_stamp_millis = time.time() # get current msg time
-		# dt = msg_stamp - prev_msg_stamp # get the time elapsed since last message
-		# prev_msg_stamp = msg_stamp
-
-		#convert seconds to elapsed to ticks
-		# ticks_since_last_msg = int(6 * float(dt*1e6) / (float(micros_per_beat) * 0.5))
+	while(time.time() - startTime < recordTime):
+		#wait for available messages, add them to slot in array
+		msg = port.poll()	
 		
-		currentTick = int(6*float(time.time()-startTime)*1e6 / (micros_per_beat))
-
-		midArr[currentTick].append(msg)s
+		if msg != None:
+			currentTick = min(143, int(6*float(time.time()-startTime)*1e6 / (micros_per_beat))) # do not allow tick to exceed max
+			print(currentTick, msg)
+			midArr[currentTick].append(msg)
 
 	buildMidiFileFromArray(midArr)
 
