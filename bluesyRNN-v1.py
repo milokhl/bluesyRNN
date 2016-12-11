8104ba8f5d1e92c2ac101d121ede757a847d14f4
Milo Knowles <mknowles@mit.edu>
2016-12-11 15:16:24 -0500

    Add function to build trY

--- bluesyRNN-v1.py
+++ bluesyRNN-v1.py
@@ -10,6 +10,7 @@ from neuralmodels.models import *
 from neuralmodels.updates import Adagrad
 from neuralmodels.costs import softmax_loss
 from neuralmodels import loadcheckpoint
+import time
 
 # import the helper library
 from midiInput import *
@@ -24,38 +25,29 @@ def buildtrX(training_solos, backing_arr, beat_arr, trX_shape):
 	Produces the training input matrix:
 	-shape: (Num_ticks x Num_examples (solos) x Feature_dim)
 	"""
-	trX = np.zeros(trX_shape, dtype=np.float)
-	#print trX
-
+	trX = np.zeros(trX_shape, dtype=np.float64)
 	# at each time step, build feature vectors for each sequence
 	for tick in range(trX_shape[0]):
-
 		# beats should repeat at some mod point (18 in this case)
 		# we need to get the current tick column from the beat array
 		currentBeatCode = beat_arr[:,tick % len(beat_arr[0])]
-		#print currentBeatCode
-
 		# iterate through all the training solos we have
 		for solo_index in range(len(training_solos)):
 
 			#build a feature vector for this solo
 			feature_vect = []
-
 			#add the current beat encoding
 			feature_vect.extend(currentBeatCode)
-
 			#add the percentage of the form completed: current_tick / total_ticks
 			feature_vect.append(float(tick) / trX_shape[2])
 			#print float(tick) / trX_shape[2]
-
 			# get the column from the backign array corresponding to current tick
 			current_backing_harmony = backing_arr[:,tick % len(backing_arr[0])]
 			#print current_backing_harmony
 			feature_vect.extend(current_backing_harmony)
-
 			# finally, add the solo state vector from the previous tick as input
 			if tick > 0:
-				prev_solo_state = training_solos[solo_index][:,tick]
+				prev_solo_state = training_solos[solo_index][:,tick-1]
 				#print "Prev solo:", prev_solo_state
 				feature_vect.extend(prev_solo_state)
 			else:
@@ -66,6 +58,14 @@ def buildtrX(training_solos, backing_arr, beat_arr, trX_shape):
 
 	return trX
 
+def buildtrY(training_solos, trY_shape):
+	trY = np.zeros(trY_shape, dtype=np.float64)
+	for tick in range(trX_shape[0]):
+		# iterate through all the training solos we have
+		for solo_index in range(len(training_solos)):
+			# add the state of the current solo at the current tick
+			trY[tick][solo_index] = training_solos[solo_index][:,tick]
+	return trY
 
 
 
@@ -96,14 +96,14 @@ feature_dim = 102
 trX_shape = (time_steps, num_sequences, feature_dim)
 
 # TODO
-midi_file_1 = 'solo1.mid'
-midi_file_2 = 'solo2.mid'
-midi_file_3 = 'solo3.mid'
-midi_file_4 = 'solo4.mid'
-midi_file_5 = 'solo5.mid'
-midi_file_6 = 'solo6.mid'
-midi_file_14 = 'solo14.mid'
-midi_file_13 = 'solo13.mid'
+midi_file_1 = './Dataset/solos/solo1.mid'
+midi_file_2 = './Dataset/solos/solo2.mid'
+midi_file_3 = './Dataset/solos/solo3.mid'
+midi_file_4 = './Dataset/solos/solo4.mid'
+midi_file_5 = './Dataset/solos/solo5.mid'
+midi_file_6 = './Dataset/solos/solo6.mid'
+midi_file_14 = './Dataset/solos/solo14.mid'
+midi_file_13 = './Dataset/solos/solo13.mid'
 trainingSolo1=convertMidiSoloToTrainingArray(midi_file_1)
 trainingSolo2=convertMidiSoloToTrainingArray(midi_file_2)
 trainingSolo3=convertMidiSoloToTrainingArray(midi_file_3)
@@ -112,14 +112,6 @@ trainingSolo5=convertMidiSoloToTrainingArray(midi_file_5)
 trainingSolo6=convertMidiSoloToTrainingArray(midi_file_6)
 trainingSolo13=convertMidiSoloToTrainingArray(midi_file_13)
 trainingSolo14=convertMidiSoloToTrainingArray(midi_file_14)
-
-# list of training solos gets passed into buildtrX
-trainingSolos = [trainingSolo1, trainingSolo2,trainingSolo3,trainingSolo4,trainingSolo5,trainingSolo6,trainingSolo13,trainingSolo14]
-trX = buildtrX(trainingSolos, backingArr, beatArr, trX_shape)
-print trX.shape
-
-
-# FINISH GATHERING TRAINING DATA #
 
 # Input vector encoding
 # [3] encoding of current beat (in 6/8 time, there are 18 triplet notes to consider)
@@ -132,18 +124,31 @@ print trX.shape
 # [86] notes held or articulated previous timestep, E1-Bflat4 possibly values (43 notes across 4 octaves)
 
 
-# create a list of layers ot pass into the model class
-# layers = [TemporalInputFeatures(inputDim),LSTM(size=512),LSTM(size=512),LSTM(size=512),softmax(outputDim)]
-# trY = T.lmatrix()
-# initial_step_size = 1e-3 # the initial learning rate
-# model = RNN(layers,softmax_loss,trY,initial_step_size,Adagrad())
-
-# # fit the model with given parameters
-# model.fitModel(trX,trY,snapshot_rate=1,path=None,epochs=30,batch_size=50,learning_rate_decay=0.97,decay_after=10)
-
 
 def main():
-	pass
+	# creating trX
+	trainingSolos = [trainingSolo1, trainingSolo2,trainingSolo3,trainingSolo4,trainingSolo5,trainingSolo6,trainingSolo13,trainingSolo14]
+	trX = buildtrX(trainingSolos, backingArr, beatArr, trX_shape)
+	print "Built training input with shape:", trX.shape
+
+	# creating trY
+	trY_shape = (time_steps, num_sequences,outputDim)
+	trY = buildtrY(trainingSolos, trY_shape)
+	print "Built training output with shape:", trY.shape
+	# FINISH GATHERING TRAINING DATA #
+
+	# create a list of layers ot pass into the model class
+	layers = [TemporalInputFeatures(feature_dim), LSTM(size=144), LSTM(size=144), softmax(outputDim)]
+	initial_step_size = 1e-3 # the initial learning rate
+
+	startTime = time.time()
+	Y = T.lmatrix()
+	model = RNN(layers,softmax_loss,Y,initial_step_size,Adagrad())
+	print "Initialized model in", time.time()-startTime, "secs."
+
+	startTrainingTime = time.time()
+	model.fitModel(trX,trY,snapshot_rate=1,path=None,epochs=3,batch_size=8,learning_rate_decay=0.97,decay_after=1)
+	print "Finished training in", time.time()-startTrainingTime, "secs."
 
 if __name__ == '__main__':
 	main()
