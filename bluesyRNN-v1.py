633d92367559ecee40123a7860f1dae0b26e95fb
Milo Knowles <mknowles@mit.edu>
2016-12-10 23:14:57 -0500

    Added more input feature setup in bluesyrnn

--- bluesyRNN-v1.py
+++ bluesyRNN-v1.py
@@ -1,6 +1,7 @@
-# first stab at Bluesy-RNN
-# By Milo Knowles
+#!/usr/bin/env 
 
+# first stab at Bluesy-RNN
+# By Milo Knowles, 2016
 
 import theano
 from theano import tensor as T
@@ -9,36 +10,63 @@ from neuralmodels.models import *
 from neuralmodels.updates import Adagrad
 from neuralmodels.costs import softmax_loss
 
+from midiInput import *
+
+# GATHER THE TRAINING DATA #
+chord_prog = [(10,'min_7',1.5), (8,'maj',1.5), (1,'maj_7',3), (3,'min_7',1.5), \
+			  (8,'dom_7',1.5), (1,'maj_7',3), (6,'maj_7',1.5), (5,'dom_7',1.5), \
+			  (10,'min_7',1.5), (8,'dom_7',1.5), (3,'min_7',1.5), (8,'dom_7',1.5), (1,'maj_7',3)]
+
+backingArr = createBackingArray(chord_prog)
+
+beatArr = getBeatArray_6_8()
+print beatArr
+
+# TODO
+# midi_file_1 = None
+# midi_file_2 = None
+# midi_file_3 = None
+# trainingSolo1=convertMidiSoloToTrainingArray(midi_file_1)
+# trainingSolo2=convertMidiSoloToTrainingArray(midi_file_2)
+# trainingSolo3=convertMidiSoloToTrainingArray(midi_file_3)
+
+# FINISH GATHERING TRAINING DATA #
+
 
 # Input vector encoding
-# [4] encoding of current beat (4 bit binary to represent 16 possible beats from 4/4 time)
+# [4] encoding of current beat (in 6/8 time, there are)
 # [1] fraction of current location in form (ex. 2nd measure of 4)
 # [1] number of measures in a phrase (to allow for trading 4s, 8s, etc)
-# [12] chord tones (-1 if not, 1 if chord tone)
-# [120] notes held or articulated previous timestep, C1-C6 possibly values (60 notes across 5 octaves)
+# [12] chord tones being played (-1 if not, 1 if chord tone)
+# [86] notes held or articulated previous timestep, E1-Bflat4  possibly values (43 notes across 4 octaves)
 
 # OUTPUT #
-# [120] notes held or articulated previous timestep, C1-C6 possibly values (60 notes across 5 octaves)
+# [86] notes held or articulated previous timestep, E1-Bflat4 possibly values (43 notes across 4 octaves)
 # [1] a silent output that blocks all other notes
 
 
 # PARAMETERS #
 inputDim = 138
+outputDim = 87
 initial_step_size = 1e-3 # the initial learning rate
 numPitchClasses = 43 # 60 different pitches, from low E on guitar to highest Bflat
 midiRange = range(40, 83) #43 notes we can choose from [40, 82]
 
 
-# wtf is this
-trY = T.lmatrix()
+# # wtf is this
+# trY = T.lmatrix()
 
-# END PARAMETERS #
-
-
-
-#create a list of layers ot pass into the model class
-layers = [TemporalInputFeatures(inputDim),LSTM(size=512),LSTM(size=512),LSTM(size=512),softmax(size=numClasses)]
+# # END PARAMETERS #
 
 
-# params: (layers,cost,Y,learning_rate,update_type=RMSprop(),clipnorm=0.0)
-model = RNN(layers,softmax_loss,trY,initial_step_size,Adagrad())
+
+# #create a list of layers ot pass into the model class
+# layers = [TemporalInputFeatures(inputDim),LSTM(size=512),LSTM(size=512),LSTM(size=512),softmax(outputDim)]
+
+# # params: (layers,cost,Y,learning_rate,update_type=RMSprop(),clipnorm=0.0)
+# model = RNN(layers,softmax_loss,trY,initial_step_size,Adagrad())
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
